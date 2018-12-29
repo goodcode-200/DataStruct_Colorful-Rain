29b657c1fedac9b25c7c9d7f386dcdcf5ab869b1
wxk <3021667395@qq.com>
2018-12-29 20:56:40 +0800

    风的效果已经俱备

--- Rain1/graphy.cpp
+++ Rain1/graphy.cpp
@@ -19,31 +19,31 @@ int sleeptime;         //函数挂起时间
 
 //这里是###接口或全局标记###定义位置，由这里定义更改各种全局参数
 //---------------------------------
-#define MAX 300	   //雨滴的总数
+#define MAX 500	   //雨滴的总数
 #define STEP 70   //雨滴的步进值（刷新时间间隔内雨滴前进的距离）
 #define WID 1      //雨滴线的宽度（从左至右）
-#define LEN  30    //雨滴线的长度（从上至下）
-#define HZ  50    //控制打雷概率的数值（数值越大，打雷的概率越低）
+#define LEN  16    //雨滴线的长度（从上至下）
+#define HZ  500   //控制打雷概率的数值（数值越大，打雷的概率越低）
 int TAG = 0;       //初始化一个tag，判断雨是否已经初始化过
+#define NUM 200  //转变风向后持续绘制的帧数，数值越大，风向改变的频率越低
+int direction = 1;     //初始化雨的降落方向，即雨的偏移方向（0,1,2）
 //---------------------------------
 
 //这里是结构体定义位置
 //---------------------------------
-typedef struct Rain {
-	int x0, y0;//雨滴的起始位置 
-	int x, y;//雨滴的终止位置 
-	int step;//步进 
+typedef struct Rain { 
 	COLORREF color;//雨滴的颜色 
 	//雨滴的绘制所需的参数 
 	int left; 
 	int top; 
 	int right; 
 	int bottom; 
-	double stangle; 
-	double endangle;
 	int Drop_tag;
 	int CLEAN_TG;      //判断是否应该清除雨   （有时是刚生成，就不必清除）
 	int ID;            //判断雨线清除样式
+	int times;         //雨滴入水后泛起涟漪的大小（倍数）
+	int direction;     //雨的撒落方向，取（0：左倾，1：直下，2：右倾）
+	int trans;         //控制标记，标记此雨滴绘制后，下次位置是否转向
 }Rain;
 Rain graph[MAX];
 typedef struct Leaf{
@@ -70,24 +70,6 @@ typedef struct Thunder
 }Thunder;
 
 
-//荷叶部分 
-void draw_leaf(int left, int top, int right, int bottom, COLORREF fillcolor, COLORREF linecolor) 
-{ 
-	//画荷叶的椭圆部分 
-	setfillcolor(fillcolor); 
-	setcolor(fillcolor); 
-	fillellipse(left, top, right, bottom); 
-	//画荷叶中的线条部分 
-	setlinecolor(linecolor); 
-	setlinestyle(PS_SOLID, 2); 
-	int ox = (left + right) / 2; 
-	int oy = (top + bottom) / 2; 
-	line(ox - 20, oy - 10, ox + 20, oy + 10); 
-	line(ox - 20, oy + 10, ox + 20, oy - 10); 
-	setlinecolor(linecolor); 
-}
-
-
 //池塘部分 
 void pool() 
 { 
@@ -98,31 +80,47 @@ void pool()
 	setfillcolor(RGB(R,G,B));
 	setcolor(RGB(R,G,B));
 	fillrectangle(0, 650, 1000, 800);
-	//绘制池塘中的荷叶 
-	/*
-	draw_leaf(450, 700, 550, 750, GREEN, BLACK);
-	draw_leaf(380, 720, 480, 770, GREEN, BLACK); 
-	*/
 }
 
 //Rain
-void InitRain(int i)
+void InitRain(int i, int de ,int tr)
 {
 	int R,G,B;
-	graph[i].x0 = rand()%1000;
+	graph[i].left = rand()%1000;
 	if(TAG==0)
 	{	
-		graph[i].y0 = rand()%(650-STEP);
+		graph[i].top = rand()%(650-STEP);
 	}
 	else	
-		graph[i].y0 = rand()%STEP;
-	graph[i].left = graph[i].x0;
-	graph[i].top = graph[i].y0;
-	graph[i].right = graph[i].left + WID;
+		graph[i].top = rand()%STEP;
+	if(tr==1)
+	{
+		if(de == 0)
+		{
+			graph[i].right = graph[i].left;
+			graph[i].direction = 1;
+		}
+		else if(de == 1)
+		{
+			graph[i].right = graph[i].left + LEN;
+			graph[i].direction = 2;
+		}
+		else if(de == 2)
+		{
+			graph[i].right = graph[i].left - LEN;
+			graph[i].direction = 0;
+		}
+	}
+	else if(tr==0)
+	{
+		if(de == 0)
+			graph[i].right = graph[i].left - LEN;
+		else if(de == 1)
+			graph[i].right = graph[i].left;
+		else if(de == 2)
+			graph[i].right = graph[i].left + LEN;
+	}
 	graph[i].bottom = graph[i].top + LEN;
-	graph[i].stangle = 4.28;
-	graph[i].endangle = 5.22;
-	graph[i].step = STEP;
 	R = rand()%255;
 	G = rand()%255;
 	B = rand()%255;
@@ -133,61 +131,117 @@ void InitRain(int i)
 //不同时刻雨的位置函数
 void Rain_quiet(int i)
 {
-	int width = 150-STEP;
-
+	int width = 140-STEP;
+	int midx,midy;   //擦除跨界雨滴时的中间值
+	int st;
 	// 擦掉原来
 	if(graph[i].CLEAN_TG)
 	{
+		int de,tr;
 		if(graph[i].Drop_tag==1)     //说明上个雨滴是圈
 		{
 			int x1,x2,y1,y2;
 			setcolor(RGB(0,0,108));
-			x1 = graph[i].left-10;
-			x2 = graph[i].left+10;
-			y1 = graph[i].bottom-5;
-			y2 = graph[i].bottom+5;
+			x1 = graph[i].right-6*graph[i].times;
+			x2 = graph[i].right+6*graph[i].times;
+			y1 = graph[i].bottom-3*graph[i].times;
+			y2 = graph[i].bottom+3*graph[i].times;
 			ellipse (x1,y1,x2,y2);
 		}
 		else                         //上个雨滴是线,擦除时注意分部分
 		{ 
 			if(graph[i].ID==2)
 			{
-				setfillcolor(0);//填充随机颜色(雨滴内) 
-				setlinecolor(0);//填充随机颜色(雨滴线) 
-				fillpie(graph[i].left,graph[i].top,graph[i].right,651,graph[i].stangle,graph[i].endangle);
-				setfillcolor(RGB(0,0,108));//填充随机颜色(雨滴内) 
-				setlinecolor(RGB(0,0,108));//填充随机颜色(雨滴线) 
-				fillpie(graph[i].left,650,graph[i].right,graph[i].bottom,graph[i].stangle,graph[i].endangle);
+				int next;
+				st = 649-graph[i].top;
+				midy = 649;
+				if(graph[i].direction==0)
+				{
+					midx = graph[i].left -st;
+					next = midx-1;
+				}
+				else if(graph[i].direction == 1)
+				{
+					midx = graph[i].left;
+				    next = midx;
+				}
+				else if(graph[i].direction == 2)
+				{
+					midx = graph[i].left + st;
+					next = midx +1;
+				}
+				setcolor(0);
+				line(graph[i].left,graph[i].top,midx,midy); 
+				setcolor(RGB(0,0,108)); 
+				line(next,midy+1,graph[i].right,graph[i].bottom);
 			}
 			else if(graph[i].ID==1)
 			{
-				setfillcolor(RGB(0,0,108));//填充随机颜色(雨滴内) 
-				setlinecolor(RGB(0,0,108));//填充随机颜色(雨滴线) 
-				fillpie(graph[i].left,graph[i].top,graph[i].right,graph[i].bottom,graph[i].stangle,graph[i].endangle);
+				setcolor(RGB(0,0,108));
+				line(graph[i].left,graph[i].top,graph[i].right,graph[i].bottom);
 			}
 			else 
-			{
-				setfillcolor(0);//填充随机颜色(雨滴内) 
-				setlinecolor(0);//填充随机颜色(雨滴线) 
-				fillpie(graph[i].left,graph[i].top,graph[i].right,graph[i].bottom,graph[i].stangle,graph[i].endangle);
+			{ 
+				setcolor(0);
+				line(graph[i].left,graph[i].top,graph[i].right,graph[i].bottom);
 			}
 		}
  
 		// 计算新位置
 		if(graph[i].Drop_tag==1)
 		{
-			InitRain(i);
+			de = graph[i].direction;
+			tr = graph[i].trans;
+			InitRain(i,de,tr);
 		}
-		else
+		else if(graph[i].trans==1)
+		{   
+			if(graph[i].direction==0)
+			{
+				graph[i].right = graph[i].left;
+				graph[i].direction = 1;
+			}
+			else if(graph[i].direction==1)
+			{
+				graph[i].right = graph[i].left + LEN;
+				graph[i].direction = 2;
+			}
+			else if(graph[i].direction==2)
+			{
+				graph[i].right = graph[i].left - LEN;
+				graph[i].direction = 0;
+			}
+		}
+		else if(graph[i].trans == 0)
 		{
-			graph[i].top += graph[i].step;
-			graph[i].bottom += graph[i].step;
+			graph[i].top += STEP;
+			graph[i].bottom += STEP;
+			if(graph[i].direction == 0)
+			{
+				graph[i].left = graph[i].left-STEP;
+				graph[i].right = graph[i].right-STEP;
+			}
+			else if(graph[i].direction == 2)
+			{
+				graph[i].left += STEP;
+				graph[i].right += STEP;
+			}	
 		}
 		
 
-		if (graph[i].bottom >= 650+(rand()%width))   //落入池塘位置随机
+		if (graph[i].bottom >= 660+(rand()%width))   //落入池塘位置随机
 		{
 			graph[i].Drop_tag = 1 ;  //设置标记，入水与否
+		}
+		if(graph[i].right<0)
+		{
+			graph[i].left += 1000;
+			graph[i].right = graph[i].left - LEN;
+		}
+		else if(graph[i].right>1000)
+		{
+			graph[i].left += -1000;
+			graph[i].right = graph[i].left + LEN;
 		}
 		if(graph[i].bottom>=650) // 设置清除雨线标记
 		{
@@ -201,27 +255,29 @@ void Rain_quiet(int i)
 	if(graph[i].Drop_tag==1)   //说明此时是入水状态，画圆圈
 	{
 		int x1,x2,y1,y2;
+		int times = rand()%3+1;
+		graph[i].times = times;
 		setcolor(graph[i].color);
-		x1 = graph[i].left-10;
-		x2 = graph[i].left+10;
-		y1 = graph[i].bottom-5;
-		y2 = graph[i].bottom+5;
+		x1 = graph[i].right-6*times;
+		x2 = graph[i].right+6*times;
+		y1 = graph[i].bottom-3*times;
+		y2 = graph[i].bottom+3*times;
 		ellipse (x1,y1,x2,y2);
 	}
 	else
 	{
 		setfillcolor(graph[i].color);//填充随机颜色(雨滴内) 
 		setlinecolor(graph[i].color);//填充随机颜色(雨滴线) 
-		fillpie(graph[i].left,graph[i].top,graph[i].right,graph[i].bottom,graph[i].stangle,graph[i].endangle);//绘制雨滴
+		line(graph[i].left,graph[i].top,graph[i].right,graph[i].bottom);
 	}
 }
 
 //Thunder
 void thunder()
 {
-    int x = rand() % 500;// +20;
-    int y = rand() % 100;//+10;
-    int stepx = rand() % 2;
+    int x = rand() % 700;// +20;
+    int y = rand() % 60;//+10;
+    int stepx = rand() % 2+1;
 	int R,G,B;
     Thunder one;
     one.x0 = x + 0 * stepx; one.y0 = y + 0 * stepx;
@@ -263,7 +319,7 @@ void thunder()
 	setpolyfillmode(WINDING);
     setfillcolor(RGB(205,173,0));
     solidpolygon(thunder, 10);//这个函数用于画填充的多边形（无边框）。void solidpolygon( const POINT *points, int num);参数：points 每个点的坐标，数组元素个数为 num。该函数会自动连接多边形首尾。num多边形顶点的个数。
-	Sleep(200);
+	Sleep(100);
     clearpolygon(thunder, 10);
 	clearrectangle(0,0,1000,650);
 	R=0;                    //打雷后池塘恢复
@@ -278,8 +334,8 @@ void thunder()
 // 主函数
 int main()
 {
-	int i;
-
+	int i,j;
+	int times;
 	srand((unsigned)time(NULL)); // 随机种子
 	initgraph(1000, 800);	// 打开图形窗口
 
@@ -288,31 +344,45 @@ int main()
 	// 初始化所有rain
 	for(i=0; i<MAX; i++)
 	{
-		InitRain(i);
+		InitRain(i,0,1);
+		graph[i].direction = direction;
 	}
 	TAG = 1;         //标记所有雨滴第一次初始化过了
 	// 绘制rain，按任意键退出
 	//PlaySound (TEXT("C:/Users/asus/Desktop/2798.wav"),NULL,SND_FILENAME | SND_ASYNC| SND_LOOP );
 	PlaySound (TEXT("Materier/10379.wav"),NULL,SND_FILENAME | SND_ASYNC| SND_LOOP );     //This is rain sound         
-
+	
 	while(!kbhit())
 	{
-		for(i=0; i<MAX; i++)  //绘制一帧雨的图片
-		{
-			Rain_quiet(i);
-			graph[i].CLEAN_TG = 1;   //标记可以清除了
+		for(j=0; j<NUM; j++)
+		{  
+			
+			for(i=0; i<MAX; i++)  //绘制一帧雨的图片
+			{
+				if (j == NUM-1)  //这是雨滴下落路径的转折点，此时，下一个绘制底部坐标只增加一个横向偏移量
+					graph[i].trans = 1;
+				else
+					graph[i].trans = 0;
+				Rain_quiet(i);     //把这滴雨绘制出来
+				graph[i].CLEAN_TG = 1;   //标记可以清除了
+			}
+				Sleep(20);
+			if (rand()%HZ==1)  //判断是否应该打雷时来降低打雷的概率
+			{
+				thunder();
+			}
+			else if(rand()%HZ==2)  //雷声激烈 ，此伏彼起
+			{ 
+				thunder();
+				thunder();
+			}
+			else if(rand()%HZ==3)  //雷声更激烈了
+			{
+				thunder();
+				thunder();
+				thunder();
+			}
 		}
-		Sleep(30);
-		if (rand()%HZ==1)  //判断是否应该打雷时来降低打雷的概率
-		{
-			thunder();
-		}
-		else if(rand()%HZ==2)  //雷声激烈 ，此伏彼起
-		{ 
-			thunder();
-			thunder();
-		}
-	
 	}
 	closegraph();    // 关闭图形窗口
 	return 0;
